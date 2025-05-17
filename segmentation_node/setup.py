e0fa835e939fb5b4875138506402c7d74ce65eaa
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-05-17 18:57:45 +0900

    update

--- segmentation_node/setup.py
+++ segmentation_node/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'segmentation_node'
 
@@ -10,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/segmentation_node_launch.py']),
+         # launch/config フォルダを含める
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
